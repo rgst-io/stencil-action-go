a35d717be851982982cb026afef5860d0a45ca58
Jared Allard <jaredallard@users.noreply.github.com>
2026-01-26 21:47:05 -0800

    chore: release v0.1.3

---
+++ shim.js
@@ -0,0 +1,30 @@
+'use strict';
+const os = require('os');
+const childProcess = require('child_process');
+
+const archMap = {
+	'x64': 'amd64',
+	'arm64': 'arm64',
+	'arm': 'arm',
+	'ia32': '386'
+};
+
+const platform = os.platform();
+const arch = os.arch();
+
+const GOOS = platform === 'win32' ? 'windows' : platform;
+const GOARCH = archMap[arch] || arch;
+
+const binary = `${__dirname}/stencil-action-${GOOS}-${GOARCH}${GOOS === 'windows' ? '.exe' : ''}`
+
+const result = childProcess.spawnSync(binary, process.argv.slice(2), {
+	stdio: 'inherit',
+	shell: false
+});
+
+if (result.error) {
+	console.error('shim: failed to spawn process:', result.error);
+	process.exit(1);
+}
+
+process.exit(result.status !== null ? result.status : 1);
