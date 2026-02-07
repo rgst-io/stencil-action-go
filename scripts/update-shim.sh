290671b8656e71186b521ff7d4f68fadc57959df
Jared Allard <jaredallard@users.noreply.github.com>
2026-02-07 18:48:33 +0700

    chore: mise w/ vscode, keep shim-config.yml

--- scripts/update-shim.sh
+++ scripts/update-shim.sh
@@ -7,11 +7,15 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 shimDir="$REPO_ROOT/shim"
 
+tmpFile=$(mktemp)
+cp -f "$shimDir/shim-config.yml" "$tmpFile"
+
 rm -rf "$shimDir"
 mkdir -p "$shimDir"
 
 gh --repo rgst-io/action-go-shim \
 	release download --dir "$shimDir" --pattern '*'
 
+cp "$tmpFile" "$shimDir/shim-config.yml"
 chmod +x "$shimDir/action-go-shim-"*
 rm -f "$shimDir/action.yml"
